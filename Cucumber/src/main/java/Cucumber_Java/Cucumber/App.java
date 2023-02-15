20b17e8d568ddf5c6789257965b2c2e21f27208b
phanitester <phanendra.u2@gmail.com>
2023-02-15 16:04:04 +0530

    My First

---
+++ Cucumber/src/main/java/Cucumber_Java/Cucumber/App.java
@@ -0,0 +1,13 @@
+package Cucumber_Java.Cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
